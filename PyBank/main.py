1fe97a99981e1f9fbe540f83c3eb79aea371513e
Igor Pavlunin <ipavlunin@gmail.com>
2021-08-26 17:22:36 -0500

    Converted lists from csv data to dictionary, solved Total Months, Total Profit values

--- PyBank/main.py
+++ PyBank/main.py
@@ -5,11 +5,9 @@ import csv
 # Reading CSV data
 csv_path = os.path.join("Resources", "budget_data.csv")
 
-total_months = []
-total_amount = []
-total_changes = []
-great_incr = []
-great_decr = []
+# Defining empty list for csv data
+date_mon = []
+prof_loss = []
 
 
 # Loading data and closing data file
@@ -19,5 +17,13 @@ with open(csv_path) as csv_file:
     # Moving cursor to the second line
     csv_header = next(csv_reader)
 
+    # Converting csv data into lists
     for row in csv_reader:
-        print(row)
+        date_mon.append(row[0])
+        prof_loss.append(row[1])
+
+dictionary = dict(zip(date_mon, prof_loss))
+dictionary = dict((k, int(v)) for k, v in dictionary.items())
+tot_months = int(len(dictionary.keys()))
+tot_profit = sum(dictionary.values())
+print(tot_profit)
