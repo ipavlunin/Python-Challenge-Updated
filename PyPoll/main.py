3b37667ef0787f145fbaa8e8ec117c309466687b
Igor Pavlunin <ipavlunin@gmail.com>
2021-08-27 15:15:25 -0500

    added code section to write to the TXT file

--- PyPoll/main.py
+++ PyPoll/main.py
@@ -48,3 +48,22 @@ print("---------------------------------------------------")
 print(f"Winner: {khan[0]}")
 print("---------------------------------------------------")
 print("```")
+
+# Output to txt file
+file_to_output = os.path.join("Analysis", "election_analysis.txt")
+l1 = "\n```text"
+l2 = "Election Results"
+l3 = "---------------------------------------------------"
+l4 = f"Total Votes: {tot_votes}"
+l5 = f"{khan[0]}: {round(khan_perc, 3)}% ({len(khan)})"
+l6 = f"{correy[0]}: {round(correy_perc, 3)}% ({len(correy)})"
+l7 = f"{li[0]}: {round(li_perc, 3)}% ({len(li)})"
+l8 = f"{otooley[0]}: {round(otooley_perc, 3)}% ({len(otooley)})"
+l9 = "---------------------------------------------------"
+l10 = f"Winner: {khan[0]}"
+l11 = "---------------------------------------------------"
+l12 = "```"
+
+output = f'{l1} \n{l2} \n{l3} \n{l4} \n{l5} \n{l6} \n{l7} \n{l8} \n{l9} \n{l10} \n{l11} \n{l12}'
+with open(file_to_output, "w") as txt_file:
+    txt_file.write(output)
